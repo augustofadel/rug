34a1ac188c76d44e14c33230ce73fa1538605543
augustofadel <augustofadel@gmail.com>
2018-03-25 19:27:41 -0300

    End cell w/o unit

--- R/ler_perfil.R
+++ R/ler_perfil.R
@@ -33,7 +33,7 @@ ler_perfil <- function(
             stringr::str_detect(paste0('\\b', var[j],'\\b'))
          pos_var <- stringr::str_locate(txt[loc], var[j])
          if (und[j] == '') {
-            pos_und <- matrix(min(35, nchar(txt[loc]) + 1), 1, 2)
+            pos_und <- matrix(min(34, nchar(txt[loc]) + 1), 1, 2)
          } else {
             pos_und <- stringr::str_locate(txt[loc], und[j])
          }
@@ -43,13 +43,13 @@ ler_perfil <- function(
                start = pos_var[2] + 1,
                end = pos_und[1] - 1
             ) %>%
-            stringr::str_replace_all('[^0-9.]', '') %>%
+            stringr::str_replace_all('[^0-9-.]', '') %>%
             stringr::str_trim('both') %>%
             as.numeric
          txt[loc] <-
             paste0(
                txt[loc] %>% stringr::str_sub(1, pos_var[1]  - 1),
-               txt[loc] %>% stringr::str_sub(pos_und[2] + 1)
+               txt[loc] %>% stringr::str_sub(pos_und[2] + max(nchar(und[j]) - 1, 0))
             )
       }
    }
